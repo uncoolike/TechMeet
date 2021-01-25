c22adb9cc7a6e0db468586ec0349307a87435dfa
uncoolike <uncoolike@gmail.com>
2021-01-25 15:54:27 -0600

    Add project files.

---
+++ TechMeet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TechMeet.MvcApplication" Language="C#" %>
---
+++ TechMeet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TechMeet.Startup))]
+namespace TechMeet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
