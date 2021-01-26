80b827d5da269fb252d8b6097cd2182da2aa4be4
uncoolike <uncoolike@gmail.com>
2021-01-26 09:07:33 -0600

    Identities added, new roles and users for those roles added to db.

--- TechMeet/Startup.cs
+++ TechMeet/Startup.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 
-[assembly: OwinStartupAttribute(typeof(TechMeet.UI.MVC.Startup))]
 namespace TechMeet.UI.MVC
 {
     public partial class Startup
